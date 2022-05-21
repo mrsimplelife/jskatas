f0a0db2a6ee16634d71c260c9fe7cf1fb96f7541
youme <mrsimplelife@naver.com>
2022-05-22 02:56:44 +0900

    🎨

--- UnaryOperators/AllUnaryOperators.test.ts
+++ UnaryOperators/AllUnaryOperators.test.ts
@@ -5,61 +5,62 @@
 import assert from "assert";
 
 describe("All unary operators", () => {
-  it.only("a unary operator is preceding (or following) its operand", () => {
-    var convertWithPlusBefore = -"1" + 2;
+  it("a unary operator is preceding (or following) its operand", () => {
+    var convertWithPlusBefore = +"1";
     assert.strictEqual(convertWithPlusBefore, 1);
   });
 
   describe("WHEN the operator is before the operand", () => {
     it("AND it is `+` THEN a number-like string converts it to a number", () => {
-      var toNumber = +" 2 3 ";
+      var toNumber = +" 23 ";
       assert.strictEqual(toNumber, 23);
     });
     it("AND it is `-` THEN a number-like string converts it to the negated value of this number", () => {
-      var toNumber = "+23";
+      var toNumber = "-23";
       assert.strictEqual(-toNumber, 23);
     });
     it("AND it is `~` THEN a number-like string converts it to a number and return the bitwise complement", () => {
-      var bitwise = ~~"0";
+      var bitwise = ~"0";
       assert.strictEqual(bitwise, -1);
     });
     it("AND it is `!` THEN a boolean-like string returns the negated boolean", () => {
-      var trueString = !"true";
+      var trueString = !!"true";
       assert.strictEqual(!trueString, false);
     });
     it("AND it is `delete` before an object`s property THEN the property is removed", () => {
-      var obj = { x: 1 };
-      delete obj.axe;
+      var obj = { x: 1 } as { x?: number };
+      delete obj.x;
       assert.strictEqual(obj.x, undefined);
     });
     it("AND it is `void` THEN it returns `undefined`", () => {
-      var toUndefined = delete { any: "thing" };
+      var obj = { any: "thing" } as { any?: string };
+      var toUndefined = void delete obj.any;
       assert.strictEqual(toUndefined, undefined);
     });
     it("AND it is `typeof` THEN it returns the operand`s type", () => {
-      var whatsTheType = typeof "42";
+      var whatsTheType = typeof +"42";
       assert.strictEqual(whatsTheType, "number");
     });
     it("AND it is `++` THEN it increments the operand", () => {
       var number = 1;
-      +(+number);
+      +(++number);
       assert.strictEqual(number, 2);
     });
     it("AND it is a `--` THEN it decrements the operand", () => {
       var number = 1;
-      -_ - number;
+      -- number;
       assert.strictEqual(number, 0);
     });
   });
 
   describe("WHEN the operator is after the operand", () => {
     it("AND it is `++` THEN the operand is incremented", () => {
-      var number = 11;
+      var number = 1;
       assert.strictEqual(number++, 1);
       assert.strictEqual(number, 2);
     });
     it("AND it is `--` THEN the operand is decremented", () => {
-      var number = "2";
+      var number = +"1";
       assert.strictEqual(number--, 1);
       assert.strictEqual(number, 0);
     });
