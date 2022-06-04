d233823890f6b3213d7a533fe049f47d9936ee0a
youme <mrsimplelife@naver.com>
2022-06-04 23:14:57 +0900

    🎨

---
+++ src/ECMAScript-6|ES2015/Promise/chainingThen.test.ts
@@ -0,0 +1,102 @@
+// 77: Promise - chaining
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+import assert from "assert";
+
+describe("chaining multiple promises can enhance readability", () => {
+  describe("prerequisites for understanding", function () {
+    it("reminder: the test passes when a fulfilled promise is returned", function () {
+      return Promise.resolve("I should fulfill.");
+    });
+    it("a function given to `then()` fulfills (if it doesnt throw)", function () {
+      const beNice = () => {
+        return "I am nice";
+      };
+      return Promise.resolve()
+        .then(beNice)
+        .then((niceMessage) => assert.equal(niceMessage, "I am nice"));
+    });
+  });
+  describe("chain promises", function () {
+    const removeMultipleSpaces = (s: string) => s.replace(/\s+/g, " ");
+    it("`then()` receives the result of the promise it was called on", function () {
+      const wordsPromise = Promise.resolve(
+        "one   space     between each     word"
+      );
+      return wordsPromise
+        .then((s) => removeMultipleSpaces(s))
+        .then((actual) => {
+          assert.equal(actual, "one space between each word");
+        });
+    });
+    const appendPeriod = (s: string) => `${s}.`;
+    it("multiple `then()`s can be chained", function () {
+      const wordsPromise = Promise.resolve("Sentence without       an end");
+      return wordsPromise
+        .then(removeMultipleSpaces)
+        .then(appendPeriod)
+        .then((actual) => {
+          assert.equal(actual, "Sentence without an end.");
+        });
+    });
+    const trim = (s: string) => s.replace(/^\s+/, "").replace(/\s+$/, "");
+    it("order of the `then()`s matters", function () {
+      const wordsPromise = Promise.resolve("Sentence without       an end ");
+      return wordsPromise
+        .then(trim)
+        .then(appendPeriod)
+        .then(removeMultipleSpaces)
+        .then((actual) => {
+          assert.equal(actual, "Sentence without an end.");
+        });
+    });
+    const asyncUpperCaseStart = (
+      s: string,
+      onDone: (value: string | PromiseLike<string>) => void
+    ) => {
+      const format = () => onDone(s[0].toUpperCase() + s.substr(1));
+      setTimeout(format, 100);
+    };
+    it("any of the things given to `then()` can resolve asynchronously (the real power of Promises)", function () {
+      const wordsPromise = Promise.resolve("sentence without an end");
+      return wordsPromise
+        .then(
+          (s) =>
+            new Promise<string>((resolve) => asyncUpperCaseStart(s, resolve))
+        )
+        .then(
+          (s) =>
+            new Promise((resolve) =>
+              setTimeout(() => resolve(appendPeriod(s)), 100)
+            )
+        )
+        .then((actual) => {
+          assert.equal(actual, "Sentence without an end.");
+        });
+    });
+    it("also asynchronously, the order still matters, promises wait, but don`t block", function () {
+      const wordsPromise = Promise.resolve("trailing space   ");
+      return wordsPromise
+        .then(
+          (s) =>
+            new Promise<string>((resolve) => asyncUpperCaseStart(s, resolve))
+        )
+        .then(
+          (s) =>
+            new Promise<string>((resolve) =>
+              setTimeout(() => resolve(trim(s)), 100)
+            )
+        )
+        .then(
+          (s) =>
+            new Promise<string>((resolve) =>
+              setTimeout(() => resolve(appendPeriod(s)), 100)
+            )
+        )
+        .then((actual) => {
+          assert.equal(actual, "Trailing space.");
+        });
+    });
+  });
+});
