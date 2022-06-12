63e4b68ae08371f62c0b34d451cde3d21e4c459a
youme <mrsimplelife@naver.com>
2022-06-12 09:46:26 +0900

    🎨

---
+++ src/ECMAScript-6|ES2015/NumberAPI/NumberIsInteger.test.ts
@@ -0,0 +1,66 @@
+// 55: Number - isInteger
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+import assert from "assert";
+
+describe("The function `Number.isInteger()`", function () {
+  it("is a static function on `Number`", function () {
+    const whatType = "function";
+    assert.equal(whatType, typeof Number.isInteger);
+  });
+  describe("handles zero in different ways", function () {
+    it("the literal `0` is an integer", function () {
+      const zero = 0;
+      assert.equal(Number.isInteger(zero), true);
+    });
+    it("the float value `0.000` is also an integer", function () {
+      const veryZero = 0.0;
+      assert.equal(Number.isInteger(veryZero), true);
+    });
+    it("the string `0` is NOT an integer", function () {
+      const stringZero = "0";
+      assert.equal(Number.isInteger(stringZero), false);
+    });
+  });
+  describe("also sees the number 1 in different ways", function () {
+    it("the sum of `0.01` and  `0.99` is an integer", function () {
+      const rest = 0.99;
+      assert.equal(Number.isInteger(0.01 + rest), true);
+    });
+    it("the sum of 0.5 + 0.6 is NOT an integer", function () {
+      const oneOrNot = 0.5 + 0.6;
+      assert.equal(Number.isInteger(oneOrNot), false);
+    });
+    it('the result of `parseInt("1")` is an integer', function () {
+      const convertedToInt = Number.parseInt("1.01");
+      assert.equal(Number.isInteger(convertedToInt), true);
+    });
+    it('the string "1" is NOT an integer', function () {
+      const stringOne = "1";
+      assert.equal(Number.isInteger(stringOne), false);
+    });
+  });
+  describe("identifies many things NOT as integer", function () {
+    it("`Number()` (which returns a 0) is an integer", function () {
+      const numberOne = Number();
+      assert.equal(Number.isInteger(numberOne), true);
+    });
+    it("the object literal `{}` is NOT an integer", function () {
+      const isInt = Number.isInteger({});
+      assert.equal(isInt, false);
+    });
+    it("the float value `0.1` is not an integer", function () {
+      const isInt = Number.isInteger(0.1);
+      assert.equal(isInt, false);
+    });
+    it("`Number.Infinity` is not an integer", function () {
+      const isInt = Number.isInteger(Infinity);
+      assert.equal(isInt, false);
+    });
+    it("`NaN` is also not an integer", function () {
+      const isInt = Number.isInteger(NaN);
+      assert.equal(isInt, false);
+    });
+  });
+});
---
+++ src/ECMAScript-6|ES2015/NumberAPI/NumberIsNaN.test.ts
@@ -0,0 +1,70 @@
+// 80: Number - isNaN
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+import assert from "assert";
+
+describe("The function `Number.isNaN()`", function () {
+  it("is a static function on `Number`", function () {
+    const whatType = "function";
+    assert.equal(whatType, typeof Number.isNaN);
+  });
+  describe("returns false", () => {
+    describe("for any not-Number type", () => {
+      it("like null", () => {
+        const justNull = null;
+        assert.equal(Number.isNaN(justNull), false);
+      });
+      it("like a string", () => {
+        const aString = "NaN";
+        assert.equal(Number.isNaN(aString), false);
+      });
+      it("like an object", () => {
+        const anObject = {};
+        assert.equal(Number.isNaN(anObject), false);
+      });
+      describe("different to the global `isNaN` function (specified in ES1)", () => {
+        it("an object gets converted to a Number before the check, and returns true therefore", () => {
+          const fn = isNaN;
+          assert.equal(fn({} as number), true);
+        });
+        it("a string gets converted to a Number first, and returns true therefore (even though its not `NaN`)", () => {
+          const fn = isNaN;
+          assert.equal(fn("just a string" as any as number), true);
+        });
+      });
+    });
+    describe("for real Numbers", () => {
+      it("like 0", () => {
+        const zero = 0;
+        assert.equal(Number.isNaN(zero), false);
+      });
+      it("or Infinity (+∞)", () => {
+        const infinity = Number.POSITIVE_INFINITY;
+        assert.equal(Number.isNaN(infinity), false);
+      });
+      it("or the biggest Number (9007199254740991 (2^53−1))", () => {
+        const max = Number.MAX_SAFE_INTEGER;
+        assert.equal(Number.isNaN(max), false);
+      });
+      it("or a decimal number", () => {
+        const pi = 3.1415926535897932;
+        assert.equal(Number.isNaN(pi), false);
+      });
+    });
+  });
+  describe("returns true for", () => {
+    it("exactly `NaN`", () => {
+      const notANumber = NaN;
+      assert.equal(Number.isNaN(notANumber), true);
+    });
+    it("the result of zero divided by zero", () => {
+      const zeroDividedByZero = 0 / 0;
+      assert.equal(Number.isNaN(zeroDividedByZero), true);
+    });
+    it("something which seems not to be a number", () => {
+      const zeroDividedByZero = Number("d");
+      assert.equal(Number.isNaN(zeroDividedByZero), true);
+    });
+  });
+});
